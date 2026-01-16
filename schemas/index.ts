04181815f24d1f4d3cfa1e43a23d48496e7957ad
adammybrim <adam@mybrim.io>
2026-01-16 11:30:40 +0000

    Fix React error by registering section types
    - Add sectionTypes to main schema index
    - Spread operator to include all section schemas
    - Resolve React error #482 in Sanity Studio

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,7 +1,9 @@
 import { schemaType } from './troubleshootingGuide'
 import { categorySchema } from './category'
+import { sectionTypes } from './sections'
 
 export const schemaTypes = [
   schemaType,
   categorySchema,
+  ...sectionTypes,
 ]
